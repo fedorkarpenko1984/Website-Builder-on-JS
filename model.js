990ca3631737b2477e34135f62a18fdf454a8230
fedorkarpenko1984 <73883259+fedorkarpenko1984@users.noreply.github.com>
2021-03-11 22:34:50 +0300

    Add block constructor
    Add blocks.js with class Block. This class help to creating templates

--- model.js
+++ model.js
@@ -1,7 +1,8 @@
+import {Block} from './classes/blocks'
 import image from './assets/image.jpg'
 
 export const model = [
-    {type: 'title', value: 'Hi from javaScript', options: {
+    new Block('title', 'Hi from javaScript', {
         tag: 'h2',
         styles: {
             background: 'linear-gradient(to right, #ff0099, #493240)',
@@ -9,40 +10,36 @@ export const model = [
             padding: '1.5rem',
             'text-align': 'center'
         }
-        }},
-
-    {type: 'image', value: image, options: {
-            styles: {
-                padding: '2rem 0',
-                display: 'flex',
-                'justify-content': 'center'
-            },
-            imageStyles: {
-                width: '500px',
-                height: 'auto',
-                'border-radius': '1rem'
-            }
-        }},
-
-    {type: 'text', value: 'Hire we go again', options: {
+    }),
+    new Block('image', image, {
+        styles: {
+            padding: '2rem 0',
+            display: 'flex',
+            'justify-content': 'center'
+        },
+        imageStyles: {
+            width: '500px',
+            height: 'auto',
+            'border-radius': '1rem'
+        }
+    }),
+    new Block('text', 'Hire we go again. Программист, стремись к чистому коду. Всегда!', {
         styles: {
             background: 'linear-gradient(to left, #f2994a, #f2c94c)',
             padding: '1rem',
             'font-weight': 'bold'
         }
-        }
-    },
-    {type: 'columns', value: [
-            'Приложение на чистом JavaScript, без фреймворков',
-            'Принципы SOLID и ООП',
-            'JavaScript - наше все'
-        ],options: {
+    }),
+    new Block('columns', [
+        'Приложение на чистом JavaScript, без фреймворков',
+        'Приложение создано с применением SOLID и ООП',
+        'JavaScript - наше все. И даже чуть больше'
+    ], {
         styles: {
             background: 'linear-gradient(to bottom, #8e2de2, #4a00e0)',
             padding: '2rem',
             color: '#fff',
             'font-weight': 'bold'
         }
-        }
-    }
+    })
 ]
