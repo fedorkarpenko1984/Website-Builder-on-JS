10bb1fed36150b19c12f036d7ddf7f916a96a698
fedorkarpenko1984 <73883259+fedorkarpenko1984@users.noreply.github.com>
2021-03-11 22:02:53 +0300

    Update
    Update utils.js, templates.js and add CSS styles to model.js

--- model.js
+++ model.js
@@ -1,10 +1,48 @@
+import image from './assets/image.jpg'
+
 export const model = [
-    {type: 'title', value: 'Hi from javaScript'},
-    {type: 'text', value: 'Hire we go again'},
+    {type: 'title', value: 'Hi from javaScript', options: {
+        tag: 'h2',
+        styles: {
+            background: 'linear-gradient(to right, #ff0099, #493240)',
+            color: '#fff',
+            padding: '1.5rem',
+            'text-align': 'center'
+        }
+        }},
+
+    {type: 'image', value: image, options: {
+            styles: {
+                padding: '2rem 0',
+                display: 'flex',
+                'justify-content': 'center'
+            },
+            imageStyles: {
+                width: '500px',
+                height: 'auto',
+                'border-radius': '1rem'
+            }
+        }},
+
+    {type: 'text', value: 'Hire we go again', options: {
+        styles: {
+            background: 'linear-gradient(to left, #f2994a, #f2c94c)',
+            padding: '1rem',
+            'font-weight': 'bold'
+        }
+        }
+    },
     {type: 'columns', value: [
-            '111111111',
-            '222222222',
-            '333333333'
-        ]},
-    {type: 'image', value: './assets/image.png'}
+            'Приложение на чистом JavaScript, без фреймворков',
+            'Принципы SOLID и ООП',
+            'JavaScript - наше все'
+        ],options: {
+        styles: {
+            background: 'linear-gradient(to bottom, #8e2de2, #4a00e0)',
+            padding: '2rem',
+            color: '#fff',
+            'font-weight': 'bold'
+        }
+        }
+    }
 ]
--- templates.js
+++ templates.js
@@ -1,20 +1,24 @@
-import {row, col} from './utils'
+import {row, col, css} from './utils'
 
 function title(block) {
-    return row(col(`<h1>${block.value}</h1>`))
+    const {tag = 'h1', styles} = block.options
+    return row(col(`<${tag}>${block.value}</${tag}>`), css(styles))
 }
 
 function text(block) {
-    return row(col(`<p>${block.value}</p>`))
+    const styles = block.options.styles
+    return row(col(`<p>${block.value}</p>`), css(styles))
 }
 
 function columns(block) {
-    const html = block.value.map(item => col(item))
-    return row(html.join(''))
+    const styles = block.options.styles
+    const html = block.value.map(col)
+    return row(html.join(''), css(styles))
 }
 
 function image(block) {
-    return row(`<img src="${block.value}" alt="">`)
+    const {imageStyles, styles} = block.options
+    return row(`<img src="${block.value}" style="${css(imageStyles)}" alt="">`, css(styles))
 }
 
 export const templates = {
