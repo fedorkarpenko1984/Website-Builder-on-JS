1c0fe54cfef3fb45593ac55de18e6ec913756dce
fedorkarpenko1984 <73883259+fedorkarpenko1984@users.noreply.github.com>
2021-03-11 18:52:00 +0300

    Add model.js for data and templates.js for HTML templates
    I transposed some code from index.js to model.js and templates.js

---
+++ model.js
@@ -0,0 +1,10 @@
+export const model = [
+    {type: 'title', value: 'Hi from javaScript'},
+    {type: 'text', value: 'Hire we go again'},
+    {type: 'columns', value: [
+            '111111111',
+            '222222222',
+            '333333333'
+        ]},
+    {type: 'image', value: './assets/image.png'}
+]
