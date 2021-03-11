a01d3d3e76f48bea35631100b1cff6e03a300adb
fedorkarpenko1984 <73883259+fedorkarpenko1984@users.noreply.github.com>
2021-03-11 14:53:59 +0300

    index.js added

---
+++ index.js
@@ -0,0 +1,15 @@
+const model = [
+    {type: 'title', value: 'Hi from javaScript'},
+    {type: 'text', value: 'Hire we go again'},
+    {type: 'columns', value: [
+        '111111111',
+        '222222222',
+        '333333333'
+        ]}
+]
+
+const $site = document.querySelector('#site')
+
+model.forEach(block => {
+    console.log(block)
+})
